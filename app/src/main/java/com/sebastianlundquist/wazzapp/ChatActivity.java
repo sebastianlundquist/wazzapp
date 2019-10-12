a0690c2f63a6075056470d5ad08627ccdca3d379
Sebastian Lundquist <sebastian.lundquist@gmail.com>
2019-10-12 11:49:31 +0200

    Add automatic loading of new messages

--- app/src/main/java/com/sebastianlundquist/wazzapp/ChatActivity.java
+++ app/src/main/java/com/sebastianlundquist/wazzapp/ChatActivity.java
@@ -3,27 +3,25 @@ package com.sebastianlundquist.wazzapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.text.style.ParagraphStyle;
+import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
-import android.widget.Toast;
 
 import com.parse.FindCallback;
-import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
-import com.parse.SaveCallback;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -36,6 +34,9 @@ public class ChatActivity extends AppCompatActivity {
 	final List<Map<String, String>> messageData = new ArrayList<>();
 	ListView chatListView;
 	SimpleAdapter simpleAdapter;
+	ParseQuery<ParseObject> query;
+	Date lastUpdated;
+	CountDownTimer timer;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +47,17 @@ public class ChatActivity extends AppCompatActivity {
 		setTitle("WazzApp: Chat with " + activeUser);
 		messageEditText = findViewById(R.id.messageEditText);
 		chatListView = findViewById(R.id.chatListView);
+		lastUpdated = new Date(0);
+		timer = new CountDownTimer(3100, 1000) {
+			@Override
+			public void onTick(long l) { }
+
+			@Override
+			public void onFinish() {
+				findNewMessages();
+				timer.start();
+			}
+		}.start();
 
 		simpleAdapter = new SimpleAdapter(ChatActivity.this, messageData, android.R.layout.simple_list_item_2, new String[] {"message", "sender"}, new int[] { android.R.id.text1, android.R.id.text2 });
 		chatListView.setAdapter(simpleAdapter);
@@ -59,25 +71,19 @@ public class ChatActivity extends AppCompatActivity {
 		List<ParseQuery<ParseObject>> queries = new ArrayList<>();
 		queries.add(query1);
 		queries.add(query2);
-		ParseQuery<ParseObject> query = ParseQuery.or(queries);
-		query.orderByAscending("createdAt");
-		query.findInBackground(new FindCallback<ParseObject>() {
-			@Override
-			public void done(List<ParseObject> objects, ParseException e) {
-				if (e == null) {
-					if (objects.size() > 0) {
-						messageData.clear();
-						for (ParseObject message : objects) {
-							Map<String, String> messageInfo = new HashMap<>();
-							messageInfo.put("message", message.getString("message"));
-							messageInfo.put("sender", message.getString("sender"));
-							messageData.add(messageInfo);
-						}
-						simpleAdapter.notifyDataSetChanged();
-					}
-				}
-			}
-		});
+		query = ParseQuery.or(queries);
+		query.orderByDescending("createdAt");
+		query.whereGreaterThan("createdAt", lastUpdated);
+		query.setLimit(10);
+		messageData.clear();
+		findNewMessages();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		if (timer != null)
+			timer.cancel();
 	}
 
 	public void sendMessage(View view) {
@@ -95,5 +101,29 @@ public class ChatActivity extends AppCompatActivity {
 			messageEditText.setText("");
 			message.saveInBackground();
 		}
+	}
+
+	public void findNewMessages() {
+		final DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Log.i("Info", dateFormat.format(lastUpdated));
+		query.whereGreaterThan("createdAt", lastUpdated);
+		lastUpdated = new Date();
+		query.findInBackground(new FindCallback<ParseObject>() {
+			@Override
+			public void done(List<ParseObject> objects, ParseException e) {
+				if (e == null) {
+					Collections.reverse(objects);
+					if (objects.size() > 0) {
+						for (ParseObject message : objects) {
+							Map<String, String> messageInfo = new HashMap<>();
+							messageInfo.put("message", message.getString("message"));
+							messageInfo.put("sender", message.getString("sender"));
+							messageData.add(messageInfo);
+						}
+						simpleAdapter.notifyDataSetChanged();
+					}
+				}
+			}
+		});
 	}
 }
